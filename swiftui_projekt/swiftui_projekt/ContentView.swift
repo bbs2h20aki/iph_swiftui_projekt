110b30b3d28a36d44a8fe67e174f8d8188fa704e
Fabian Kirchmann / BBS2H20A <Fabian.Kirchmann@edu.bib.de>
2022-01-24 08:57:46 +0100

    initial commit

---
+++ swiftui_projekt/swiftui_projekt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftui_projekt
+//
+//  Created by Fabian Kirchmann / BBS2H20A on 24.01.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftui_projekt/swiftui_projekt/swiftui_projektApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_projektApp.swift
+//  swiftui_projekt
+//
+//  Created by Fabian Kirchmann / BBS2H20A on 24.01.22.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_projektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
