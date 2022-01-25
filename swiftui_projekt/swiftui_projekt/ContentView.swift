ac245ee6f929396d081f8705c870bea28e6b15da
Fabian Kirchmann / BBS2H20A <Fabian.Kirchmann@edu.bib.de>
2022-01-25 08:58:31 +0100

    content view

--- swiftui_projekt/swiftui_projekt/ContentView.swift
+++ swiftui_projekt/swiftui_projekt/ContentView.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 
 struct ContentView: View {
+    
+    @State var books = [Book]()
+    
     var body: some View {
         Text("Hello, world!")
             .padding()
+            .onAppear() {
+                Api().loadData { (books) in
+                    self.books = books
+                }
+            }.navigationTitle("Book List")
     }
 }
 
